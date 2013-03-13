b5fa0897044450d45433ccd63eece651c558be30
Philip Collin <lyeeedar@gmail.com>
2013-03-13 00:14:02 +0000

    Timelines added!

--- ParticleEditor/src/com/lyeeedar/ParticleEditor/Main.java
+++ ParticleEditor/src/com/lyeeedar/ParticleEditor/Main.java
@@ -2,12 +2,16 @@ package com.lyeeedar.ParticleEditor;
 
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
@@ -18,6 +22,9 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -31,7 +38,9 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
@@ -51,6 +60,10 @@ import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Json;
 import com.lyeeedar.Graphics.ParticleEffects.ParticleEffect;
 import com.lyeeedar.Graphics.ParticleEffects.ParticleEmitter;
+import com.lyeeedar.Graphics.ParticleEffects.ParticleEmitter.ParticleAttribute;
+import com.lyeeedar.Graphics.ParticleEffects.ParticleEmitter.TimelineFloat;
+import com.lyeeedar.Graphics.ParticleEffects.ParticleEmitter.TimelineInteger;
+import com.lyeeedar.Graphics.ParticleEffects.ParticleEmitter.TimelineValue;
 import com.lyeeedar.Roguelike3D.Graphics.Lights.LightManager;
 import com.lyeeedar.Roguelike3D.Graphics.Lights.LightManager.LightQuality;
 
@@ -89,6 +102,46 @@ public class Main extends JFrame {
 		setVisible(true);
 		
 		right();
+		timeline();
+	}
+	
+	public void timeline()
+	{
+		bottom.removeAll();
+		bottom.setLayout(new GridLayout(1, 1));
+		
+		JTabbedPane tabs = new JTabbedPane();
+		
+		bottom.add(tabs);
+		
+		bottom.revalidate();
+		bottom.repaint();
+		
+		if (renderer.currentEmitter == -1) return;
+		
+		TimelinePanel<TimelineInteger> spriteTimeline = new TimelinePanel<TimelineInteger>(ParticleAttribute.SPRITE, TimelineInteger.class, renderer.effect.getEmitter(renderer.currentEmitter));
+		JScrollPane spriteScroll = new JScrollPane(spriteTimeline);
+		spriteScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		
+		TimelinePanel<TimelineFloat> sizeTimeline = new TimelinePanel<TimelineFloat>(ParticleAttribute.SIZE, TimelineFloat.class, renderer.effect.getEmitter(renderer.currentEmitter));
+		JScrollPane sizeScroll = new JScrollPane(sizeTimeline);
+		sizeScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		
+		TimelinePanel<TimelineFloat> colourTimeline = new TimelinePanel<TimelineFloat>(ParticleAttribute.COLOUR, TimelineFloat.class, renderer.effect.getEmitter(renderer.currentEmitter));
+		JScrollPane colourScroll = new JScrollPane(colourTimeline);
+		colourScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		
+		TimelinePanel<TimelineFloat> velocityTimeline = new TimelinePanel<TimelineFloat>(ParticleAttribute.VELOCITY, TimelineFloat.class, renderer.effect.getEmitter(renderer.currentEmitter));
+		JScrollPane velocityScroll = new JScrollPane(velocityTimeline);
+		velocityScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		
+		tabs.addTab("Sprite", spriteScroll);
+		tabs.addTab("Size", sizeScroll);
+		tabs.addTab("Colour", colourScroll);
+		tabs.addTab("Velocity", velocityScroll);
+		
+		bottom.revalidate();
+		bottom.repaint();
 	}
 	
 	public void right()
@@ -145,6 +198,7 @@ public class Main extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				renderer.currentEmitter = comboBox.getSelectedIndex();
 				right();
+				timeline();
 			}});
 		
 		JButton newEmitter = new JButton("New");
@@ -154,6 +208,7 @@ public class Main extends JFrame {
 				ParticleEmitter emitter = renderer.getDefaultEmitter();
 				renderer.effect.addEmitter(emitter, 0, 0, 0);
 				right();
+				timeline();
 			}});
 		
 		JButton deleteEmitter = new JButton("Delete");
@@ -163,6 +218,7 @@ public class Main extends JFrame {
 				renderer.effect.deleteEmitter(comboBox.getSelectedIndex());
 				renderer.currentEmitter = -1;
 				right();
+				timeline();
 			}});
 		
 		JButton renameEmitter = new JButton("Rename");
@@ -295,25 +351,32 @@ public class Main extends JFrame {
 		gc.gridy++;
 		
 		panel.add(new JLabel("Blend Mode:"), gc);
+		
 		String[] blendModes = {"ZERO", "ONE",
 				"SRC_COLOR", "ONE_MINUS_SRC_COLOR", "DST_COLOR", "ONE_MINUS_DST_COLOR",
 				"SRC_ALPHA", "ONE_MINUS_SRC_ALPHA", "DST_ALPHA", "ONE_MINUS_DEST_ALPHA",
 				"CONSTANT_COLOR", "ONE_MINUS_CONSTANT_COLOR", "CONSTANT_ALPHA", "ONE_MINUS_CONSTANT_ALPHA",
 				"SRC_ALPHA_SATURATE"};
 		
-		JPanel bm = new JPanel();
-		bm.add(new JLabel("SRC:"));
 		final JComboBox<String> SRCBlend = new JComboBox<String>(blendModes);
 		SRCBlend.setSelectedItem(getBlendString(renderer.effect.getEmitter(renderer.currentEmitter).blendFuncSRC));
-		bm.add(SRCBlend);
 		
-		bm.add(new JLabel("DST:"));
 		final JComboBox<String> DSTBlend = new JComboBox<String>(blendModes);
 		DSTBlend.setSelectedItem(getBlendString(renderer.effect.getEmitter(renderer.currentEmitter).blendFuncDST));
-		bm.add(DSTBlend);
+		
+		gc.gridy++;
+		gc.gridx = 0;
+		panel.add(new JLabel("SRC:"), gc);
 		
 		gc.gridx = 1;
-		panel.add(bm, gc);
+		panel.add(SRCBlend, gc);
+		
+		gc.gridx = 0;
+		gc.gridy++;
+		panel.add(new JLabel("DST:"), gc);
+		
+		gc.gridx = 1;
+		panel.add(DSTBlend, gc);
 		
 		gc.gridx = 0;
 		gc.gridy++;
@@ -690,19 +753,23 @@ class Renderer implements ApplicationListener
         cam.update();
 	}
 
+	ArrayList<ParticleEmitter> emitters = new ArrayList<ParticleEmitter>();
 	@Override
 	public void render() {
 		
 		Gdx.graphics.getGL20().glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 		Gdx.graphics.getGL20().glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 		
+		emitters.clear();
+		
+		effect.setPosition(0, -2, 10);
+		effect.update(Gdx.app.getGraphics().getDeltaTime(), cam);
+		effect.getEmitters(emitters, cam);
+		
+		Collections.sort(emitters, ParticleEmitter.getComparator());
+
 		ParticleEmitter.begin(cam);
-		if (effect != null)
-		{
-			effect.setPosition(0, -2, 10);
-			effect.update(Gdx.app.getGraphics().getDeltaTime(), cam);
-			effect.render();
-		}
+		for (ParticleEmitter pe : emitters) pe.render();
 		ParticleEmitter.end();
 		
 		Gdx.graphics.getGL20().glDisable(GL20.GL_CULL_FACE);
@@ -727,4 +794,227 @@ class Renderer implements ApplicationListener
 	public void dispose() {
 	}
 	
+}
+
+class TimelinePanel<T extends TimelineValue> extends JPanel implements MouseListener, MouseMotionListener
+{
+	static final int top = 20;
+	static final int bot = 30;
+	
+	static final int startOffset = 15;
+	
+	static final int time = 10;
+	
+	static final int blobw = 8;
+	static final int blobh = 8;
+	
+	ArrayList<T> values;
+	Class<T> type;
+	ParticleEmitter emitter;
+	
+	int selectedIndex = -1;
+	
+	boolean lock = false;
+	
+	ParticleAttribute attribute;
+	public TimelinePanel(ParticleAttribute attribute, Class<T> type, ParticleEmitter emitter)
+	{
+		this.emitter = emitter;
+		this.type = type;
+		this.attribute = attribute;
+		this.values = getValue();
+		
+		setPreferredSize(new Dimension((time*10*50)+startOffset, 100));
+		setMinimumSize(new Dimension((time*10*50)+startOffset, 100));
+		setSize((time*10*50)+startOffset, 100);
+		
+		addMouseListener(this);
+		addMouseMotionListener(this);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public ArrayList<T> getValue()
+	{
+		ArrayList<T> values = new ArrayList<T>();
+		if (attribute == ParticleAttribute.SPRITE)
+		{
+			for (TimelineInteger t : emitter.getSpriteTimeline()) values.add((T) t.copy());
+		}
+		else if (attribute == ParticleAttribute.SIZE)
+		{
+			for (TimelineFloat t : emitter.getSizeTimeline()) values.add((T) t.copy());
+		}
+		else if (attribute == ParticleAttribute.COLOUR)
+		{
+			for (TimelineFloat t : emitter.getColourTimeline()) values.add((T) t.copy());
+		}
+		else if (attribute == ParticleAttribute.VELOCITY)
+		{
+			for (TimelineFloat t : emitter.getVelocityTimeline()) values.add((T) t.copy());
+		}
+		
+		return values;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void writeValues()
+	{
+		sortValues();
+		if (attribute == ParticleAttribute.SPRITE)
+		{
+			emitter.setSpriteTimeline((List<TimelineInteger>) values);
+			emitter.reloadTextures();
+		}
+		else if (attribute == ParticleAttribute.SIZE)
+		{
+			emitter.setSizeTimeline((List<TimelineFloat>) values);
+		}
+		else if (attribute == ParticleAttribute.COLOUR)
+		{
+			emitter.setColourTimeline((List<TimelineFloat>) values);
+		}
+		else if (attribute == ParticleAttribute.VELOCITY)
+		{
+			emitter.setVelocityTimeline((List<TimelineFloat>) values);
+		}
+	}
+	
+	@Override
+	public void paint(Graphics g)
+	{
+		super.paint(g);
+		
+		g.setColor(java.awt.Color.WHITE);
+		
+		int x = 0;
+		for (float i = 0; i < time; i+=0.1f, x++)
+		{
+			g.drawLine(startOffset+(x*50), top, startOffset+(x*50), bot+20);
+			for (int subx = 1; subx < 10; subx++)
+			{
+				g.drawLine(startOffset+(x*50)+(subx*5), top, startOffset+(x*50)+(subx*5), bot);
+			}
+			int val = (int)(i*10);
+			float fval = ((float)val)/10;
+			g.drawString(""+fval, startOffset+(x*50)-10, bot+20+15);
+		}
+		
+		g.setColor(java.awt.Color.RED);
+		
+		for (int i = 0; i < values.size(); i++)
+		{
+			if (i == selectedIndex)
+			{
+				g.fillRect((int) (startOffset+(values.get(i).time*500)-(blobw/2)-2), top+10-(blobh/2)-2, blobw+4, blobh+4);
+			}
+			else
+				g.fillRect((int) (startOffset+(values.get(i).time*500)-(blobw/2)), top+10-(blobh/2), blobw, blobh);
+		}
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		if (selectedIndex > 0)
+		{
+			values.get(selectedIndex).time = ((float)(e.getX()-startOffset)) / 500;
+			repaint();
+		}
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		if (lock) return;
+		for (int i = 0; i < values.size(); i++)
+		{
+			int pos = (int) (startOffset+(values.get(i).time*500)-(blobw/2));
+			if (e.getX() > pos && e.getX() < pos+blobw &&
+					e.getY() > top+10-(blobh/2) && e.getY() < top+10+blobh)
+			{
+				selectedIndex = i;
+				repaint();
+				return;
+			}
+		}
+		selectedIndex = -1;
+		repaint();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if (selectedIndex != -1)
+		{
+			//doshit
+		}
+		else
+		{
+			float time = ((float)(e.getX()-startOffset)) / 500;
+			values.add(getNewT(time, values.get(0).values.length));
+			writeValues();
+			repaint();
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		lock = true;
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		
+		if (selectedIndex != -1)
+		{
+			writeValues();
+		}
+		
+		lock = false;
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void sortValues()
+	{
+		Collections.sort(values, new Comparator<T>(){
+			@Override
+			public int compare(T o1, T o2) {
+				return (int) ((o1.time - o2.time)*100);
+			}});
+		
+		for (int i = 0; i < values.size()-1; i++)
+		{
+			T t = values.get(i);
+			if (t.interpolated)
+			{
+				t.setInterpolated(true, values.get(i+1));
+			}
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public T getNewT(float time, int numValues)
+	{
+		if (type == TimelineInteger.class)
+		{
+			Integer[] values = new Integer[numValues];
+			for (int i = 0; i < values.length; i++) values[i] = 0;
+			
+			return (T) new TimelineInteger(time, values);
+		}
+		else if (type == TimelineFloat.class)
+		{
+			Float[] values = new Float[numValues];
+			for (int i = 0; i < values.length; i++) values[i] = 0f;
+			
+			return (T) new TimelineFloat(time, values);
+		}
+		
+		return null;
+	}
 }
